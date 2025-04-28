99511431a84ab432adb5fc725f58629be654ebc2
developer-sultan <sultans.blogs@gmail.com>
2025-04-28 23:05:12 +0300

    file: add json file for dynamic

---
+++ index.js
@@ -0,0 +1,12 @@
+const summaryCard = document.querySelector(".summary-stat_container");
+
+summaryCard.innerHTML = ` <div class="summary-card_stat summary-card_stat-reaction">
+        <div>
+          <img src="assets/images/icon-reaction.svg" alt="No-icon">
+          <span>text</span>
+        </div>
+        <div>
+          <span>25&nbsp;</span>
+          <span>/ 100</span>
+        </div>
+       </div>`;

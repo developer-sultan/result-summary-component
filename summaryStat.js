d46aef2bf60b0217a9a505d3c49011e3f87f1ac7
developer-sultan <sultans.blogs@gmail.com>
2025-05-03 22:00:00 +0300

    err: error fixed

--- summaryStat.js
+++ summaryStat.js
@@ -1,24 +1,24 @@
 const data = [
   {
-    "className" : ".summary-card_stat-reaction",
+    "className" : "summary-card_stat-reaction",
     "category": "Reaction",
     "score": 80,
     "icon": "./assets/images/icon-reaction.svg"
   },
   {
-    "className" : ".summary-card_stat-memory",
+    "className" : "summary-card_stat-memory",
     "category": "Memory",
     "score": 92,
     "icon": "./assets/images/icon-memory.svg"
   },
   {
-    "className" : ".summary-card_stat-verbal",
+    "className" : "summary-card_stat-verbal",
     "category": "Verbal",
     "score": 61,
     "icon": "./assets/images/icon-verbal.svg"
   },
   {
-    "className" : ".summary-card_stat-visual",
+    "className" : "summary-card_stat-visual",
     "category": "Visual",
     "score": 72,
     "icon": "./assets/images/icon-visual.svg"
